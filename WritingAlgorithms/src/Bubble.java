84d8c7d171cb5ac52bcd3cded86285854fd0d071
yeremygarcia <jeremyisgarcia@gmail.com>
2023-05-30 22:41:37 -0400

    1

---
+++ WritingAlgorithms/src/Bubble.java
@@ -0,0 +1,23 @@
+public class Bubble {
+    public static void main(String[] args) {
+        int[] array = {8, 6, 7, 5, 3, 0, 9};
+
+        bubbleSort(array);
+
+        for(int i : array) {
+            System.out.print(i);
+        }
+    }
+    public static void bubbleSort(int[] array) {
+
+        for(int i = 0; i < array.length - 1; i++) {
+            for(int j = 0; j < array.length - i - 1; j++) {
+                if(array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ WritingAlgorithms/src/Quick.java
@@ -0,0 +1,40 @@
+public class Quick{
+    public static void main(String[] args)
+    {
+        int[] array = {8, 6, 7, 5, 3, 0, 9};
+
+        quickSort(array, 0, array.length - 1);
+
+        for(int i : array){
+            System.out.print(i + " ");
+        }
+    }
+    private static void quickSort(int[] array, int start, int end) {
+
+        if(end <= start) return;
+
+        int pivot = partition(array, start, end);
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
+    }
+    private static int partition(int[] array, int start, int end) {
+
+        int pivot = array[end];
+        int i = start - 1;
+
+        for(int j = start; j <= end; j++) {
+            if(array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i++;
+        int temp = array[i];
+        array[i] = array[end];
+        array[end] = temp;
+
+        return i;
+    }
+}
